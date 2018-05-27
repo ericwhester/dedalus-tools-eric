c3fa4adca0d388fbfb6c80fad13bf8ea828ad443
Eric Hester <eric.w.hester@icloud.com>
2018-05-27 18:46:53 +1000

    plot_tools.py: fixed axes settings.

--- plot_tools.py
+++ plot_tools.py
@@ -8,9 +8,14 @@ def logmag(arr): return np.log10(np.abs(arr))
 
 # Plotting 1D data
 
-def ax_settings(ax,**axkwargs):
+def ax_settings(ax,xlim=None, ylim=None, aspect=None, xlabel=None, ylabel=None, title=None):
     """Apply kwargs to axis."""
-    ax.set(**axkwargs)
+    if xlim: ax.set(xlim=xlim)
+    if ylim: ax.set(ylim=ylim)
+    if aspect: ax.set(aspect=aspect)
+    if xlabel: ax.set(xlabel=xlabel)
+    if ylabel: ax.set(ylabel=ylabel)
+    if title: ax.set(title=title)
 
 def plot1D(x,arr,
            fig=None, ax=None, savename=None, figsize=None,
@@ -19,7 +24,7 @@ def plot1D(x,arr,
     """Simple 1D plot."""
     if not ax: fig, ax = plt.subplots(figsize=figsize)
     plot = ax.plot(x, arr, **kwargs)
-    ax.set(xlim=xlim,ylim=ylim,aspect=aspect,xlabel=xlabel,ylabel=ylabel,title=title)
+    ax_settings(ax, xlim=xlim,ylim=ylim,aspect=aspect,xlabel=xlabel,ylabel=ylabel,title=title)
     if savename: plt.savefig(savename,bbox_inches='tight')
     return fig, ax
 
@@ -33,7 +38,7 @@ def cplot(x1,x2,arr,
     if not ax: fig, ax = plt.subplots(figsize=figsize)
     if len(x1.shape) == 1: x1, x2 = np.meshgrid(x1,x2,indexing='ij')
     plot = ax.pcolormesh(x1,x2,arr,**kwargs)
-    ax.set(xlim=xlim,ylim=ylim,aspect=aspect,xlabel=xlabel,ylabel=ylabel,title=title)
+    ax_settings(ax, xlim=xlim,ylim=ylim,aspect=aspect,xlabel=xlabel,ylabel=ylabel,title=title)
     if colorbar: plt.colorbar(plot,fraction=0.046,pad=0.04,shrink=.7)
     if savename: plt.savefig(savename,dpi=dpi,bbox_inches='tight')
     return fig, ax
@@ -45,7 +50,7 @@ def coeff_plot(array,fig=None,ax=None,savename=None,dpi=200,
     fig, ax = plt.subplots()
     kx, ky = np.arange(array.shape[0]), np.arange(array.shape[1])
     plot = ax.pcolormesh(kx,ky,logmag(array).T,vmin=vmin,vmax=vmax,**kwargs) 
-    ax.set(xlim=xlim,ylim=ylim,aspect=aspect,xlabel=xlabel,ylabel=ylabel,title=title)
+    ax_settings(ax, xlim=xlim,ylim=ylim,aspect=aspect,xlabel=xlabel,ylabel=ylabel,title=title)
     if colorbar: cbar = plt.colorbar(plot)
     if reverse: cbar.ax.invert_yaxis()
     if savename: plt.savefig(savename,dpi=dpi,bbox_inches='tight')
@@ -57,7 +62,7 @@ def extend_angle(*arrays):
     return [np.concatenate([arr,arr[[0],:]],axis=0) for arr in arrays]
     
 def polar_plot(θθ,rr,array,
-               fig=None,ax=None,save=False,colorbar=True,
+               fig=None,ax=None,savename=False,dpi=200,colorbar=True,
                return_plot=False,**kwargs):
     """Wrapper to create a polar plot of a quantity."""
     if fig==None: fig, ax = plt.subplots(figsize=(4,6),subplot_kw=dict(projection='polar'))
@@ -75,12 +80,12 @@ def polar_plot(θθ,rr,array,
         else: 
             cax = fig.add_axes([.12,.3,.78,.03])
             cbar = plt.colorbar(plot,cax=cax,orientation='horizontal')
-    if save != False: plt.savefig(save,dpi=400,bbox_inches='tight')
+    if savename: plt.savefig(savename,dpi=dpi,bbox_inches='tight')
     if return_plot: return fig, ax, plot
     return fig, ax    
     
 def polar_vel_plot(θθ,rr,ur, uθ,
-                   sel=None,fig=None,ax=None,save=False,
+                   sel=None,fig=None,ax=None,savename=False,dpi=200,
                    double=False,ticks=False,grid=True,**kwargs):
     """Wrapper for a polar quiver plot."""
     if sel==None: sel = (slice(0,-1,8),slice(0,-1,8))
@@ -97,7 +102,7 @@ def polar_vel_plot(θθ,rr,ur, uθ,
         ax.set_thetamax(180)
     if grid: ax.grid()
     plt.colorbar(plot,ax=ax,orientation='vertical')
-    if save != False: plt.savefig(save,dpi=400,bbox_inches='tight')    
+    if savename: plt.savefig(savename,dpi=dpi,bbox_inches='tight')    
     return fig, ax    
 
 # Plotting immersed boundaries
